fb35c148ae470e89f56908543daf98cfa3bbfd3e
Patrick_Cabajes <patrickcabajes07@gmail.com>
2024-10-24 01:37:47 +0800

    My pseudocode

---
+++ user_input_name_and_age_info_collector_and_oldest_individual_identifier.py
@@ -0,0 +1,25 @@
+#declare personalinfovault as an empty dictionary array
+#while True:
+    #ask user to input name (i.e Pat)
+    #add conditions such as it contains only letters, initial letter is capitalized while others are in lower case
+    #if not:
+        #print "Error: Please input a valid name and follow the format"
+
+#while True:
+    #ask user to input age 
+    #add conditions such as age >= 0
+    #if not:
+        #print "Error: Please input a valid age"
+
+    #add the user input name and user input age to the dictionary
+
+    #Ask user if he/she would like to input another entry
+    #if the response is no
+        #print the name and age of the oldest individual 
+
+#declare oldest age as 0
+#declare oldest name as a blank string
+    #for each entry in personalinfovaul set
+        #entry.age > oldest age
+        #oldest age > entry.age
+        #oldest name > entry.name
