fb8b76ae2d9d18e60d7ff802d56511161e289677
Patrick_Cabajes <patrickcabajes07@gmail.com>
2024-10-24 18:00:20 +0800

    I'm having some issue with the if conditions of the name

--- user_input_name_and_age_info_collector_and_oldest_individual_identifier.py
+++ user_input_name_and_age_info_collector_and_oldest_individual_identifier.py
@@ -1,9 +1,28 @@
-#declare personalinfovault as an empty dictionary array
+#declare personalinfovault as an empty array
+personalinfovault = {}
+
 #while True:
     #ask user to input name (i.e Pat)
+digit = False
+while True:
+    name = input("Please input your name: ")
+
     #add conditions such as it contains only letters, initial letter is capitalized while others are in lower case
     #if not:
         #print "Error: Please input a valid name and follow the format"
+    if not name.istitle():
+        print("All characters should be in lowercase except the initial letter")
+
+    for char in name:
+        if name.isdigit():
+                digit = True
+                break
+    if digit == True:
+        print("Please put letters only")
+        
+    if len(name) <2:
+        print("Please provide a longer name")
+
 
 #while True:
     #ask user to input age 
