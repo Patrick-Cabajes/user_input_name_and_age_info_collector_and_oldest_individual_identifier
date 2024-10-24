c2040288ec9263936aac7c82206e8061311eaa30
Patrick_Cabajes <patrickcabajes07@gmail.com>
2024-10-24 19:59:21 +0800

    I have fixed the first issue on the loop for name. However, I am experiencing another problem with the conditions for age.

--- user_input_name_and_age_info_collector_and_oldest_individual_identifier.py
+++ user_input_name_and_age_info_collector_and_oldest_individual_identifier.py
@@ -3,32 +3,38 @@ personalinfovault = {}
 
 #while True:
     #ask user to input name (i.e Pat)
-digit = False
 while True:
-    name = input("Please input your name: ")
+    while True:
+        name = input("Please input your name: ")
 
-    #add conditions such as it contains only letters, initial letter is capitalized while others are in lower case
-    #if not:
-        #print "Error: Please input a valid name and follow the format"
-    if not name.istitle():
-        print("All characters should be in lowercase except the initial letter")
-
-    for char in name:
-        if name.isdigit():
-                digit = True
-                break
-    if digit == True:
-        print("Please put letters only")
+        #add conditions such as it contains only letters, initial letter is capitalized while others are in lower case
+        #if not:
+            #print "Error: Please input a valid name and follow the format"
+        if not name.isalpha():
+            print("Error: Please input letter only")
+    
+        elif not name.istitle():
+            print("Error: All characters should be in lowercase except the initial letter")
+     
+        elif len(name) <2:
+            print("Error: Please provide a longer name")
         
-    if len(name) <2:
-        print("Please provide a longer name")
+        else:
+            break
 
+    #while True:
+        #ask user to input age
+    while True:
+        age = int(input("How old are you?: "))
 
-#while True:
-    #ask user to input age 
-    #add conditions such as age >= 0
-    #if not:
-        #print "Error: Please input a valid age"
+        #add conditions such as age >= 0
+        #if not:
+            #print "Error: Please input a valid age"
+        if age < 0 and age > 150:
+            print("Please provide a valid age")
+
+        else:
+            break
 
     #add the user input name and user input age to the dictionary
 
