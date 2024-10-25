92dd3b89c534767f9c603704d1690f443d323c6d
Patrick_Cabajes <patrickcabajes07@gmail.com>
2024-10-25 15:32:05 +0800

    the loop finally works properly

--- user_input_name_and_age_info_collector_and_oldest_individual_identifier.py
+++ user_input_name_and_age_info_collector_and_oldest_individual_identifier.py
@@ -5,49 +5,58 @@ personalinfovault = {}
     #ask user to input name (i.e Pat)
 while True:
     while True:
-        name = input("Please input your name: ")
-        #add conditions such as it contains only letters, initial letter is capitalized while others are in lower case
-        #if not:
-            #print "Error: Please input a valid name and follow the format"
-        if not name.isalpha():
-            print("Error: Please input letter only")
+        while True:
+            name = input("Please input your name: ")
+            #add conditions such as it contains only letters, initial letter is capitalized while others are in lower case
+            #if not:
+                #print "Error: Please input a valid name and follow the format"
+            if not name.isalpha():
+                print("Error: Please input letter only")
     
-        elif not name.istitle():
-            print("Error: All characters should be in lowercase except the initial letter")
+            elif not name.istitle():
+                print("Error: All characters should be in lowercase except the initial letter")
      
-        elif len(name) <2:
-            print("Error: Please provide a longer name")
+            elif len(name) <2:
+                print("Error: Please provide a longer name")
         
-        else:
-            break
+            else:
+                break
 
-    #while True:
-        #ask user to input age
-    while True:
-        age = int(input("How old are you?: "))
-        #add conditions such as age >= 0
-        #if not:
-            #print "Error: Please input a valid age"
-        if age < 0 or age > 150:
-            print("Please provide a valid age between 0 and 150")
+        #while True:
+            #ask user to input age
+        while True:
+            age = int(input("How old are you?: "))
+            #add conditions such as age >= 0
+            #if not:
+                #print "Error: Please input a valid age"
+            if age < 0 or age > 150:
+                print("Error: Please provide a valid age between 0 and 150")
 
-        else:
-            break
+            else:
+                break
     
-    personalinfovault = {
-        "name" : name,
-        "age"  : age,
-    }
-    
+        #Store the user's information to the dictionary
+        user_information = {
+            "name" : name,
+            "age"  : age,
+        }
 
-    #add the user input name and user input age to the dictionary
-
-    #Ask user if he/she would like to input another entry
+        #Ask user if he/she would like to input another entry
+        retry = input("Would you like to input another entry? (yes/no): ")
+        break
+        
+    if retry == "yes":
+        print("Request Accepted: Please input a new entry")
+        
     #if the response is no
-        #print the name and age of the oldest individual 
+        #print the name and age of the oldest individual
+    elif retry == "no":
+        print("Request Accepted: Please exit")
+        break
 
 #declare oldest age as 0
 #declare oldest name as a blank string
+
     #for each entry in personalinfovaul set
         #entry.age > oldest age
         #oldest age > entry.age
